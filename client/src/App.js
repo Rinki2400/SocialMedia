80d227380e9d488127db0a66c82848bdd647ba88
RinkiShara <rinki2001sharma@gmail.com>
2025-07-04 16:47:12 +0530

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import memories from "./assets/24af27424e0f0b1d83d00c182b5eaa9d.jpg";
+import Form from "./component/Form/Form";
+import Posts from "./component/Posts/Posts";
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <div className="container">
+      <div className="app-bar">
+        <h2 className="heading">Memories</h2>
+        <img src={memories} alt="memories" className="memoimg" />
+      </div>
+      <div className="main_container">
+        <div className="grid-item">
+          <Posts />
+        </div>
+        <div className="grid-item">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/component/Posts/post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+      <h1>Pos1 Inkit</h1>
+    </div>
+  )
+}
+
+export default Post
